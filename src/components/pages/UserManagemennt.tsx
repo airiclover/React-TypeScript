c5f047e40f990c7642b13271d072836b862ad1ae
airi <airi-love-clover@rakuten.jp>
2021-04-15 23:55:14 +0900

    ✨ Updata files.

--- src/components/pages/UserManagemennt.tsx
+++ src/components/pages/UserManagemennt.tsx
@@ -17,7 +17,6 @@ export const UserManagement: VFC = memo(() => {
   const { getUsers, loading, users } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
   const { loginUser } = useLoginUser();
-  console.log(loginUser);
 
   useEffect(() => getUsers(), []);
 
@@ -49,7 +48,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
     </>
   );
 });
